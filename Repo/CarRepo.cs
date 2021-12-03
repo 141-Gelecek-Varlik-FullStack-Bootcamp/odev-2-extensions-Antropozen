822169a96ded34c7565cd4f93659c39d55dc4e30
Enes Gökhan Özcan <enes.gokhan.ozcan@gmail.com>
2021-12-03 22:00:21 +0300

    interface added.

---
+++ Repo/CarRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    interface  IVehicle
+    {
+        int Id { get; set; }
+        string Brand { get; set; }
+        string Series { get; set; }
+        string Model { get; set; }
+        int Year { get; set; }
+        string Fuel { get; set; }
+        string Gear { get; set; }
+        int KM { get; set; }
+        int EnginePower { get; set; }
+        int EngineCapacity { get; set; }
+        int Door { get; set; }
+    }
+}
---
+++ Repo/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    public class Motorcycle : IVehicle
+    {
+        //IVehicle interface added.
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Brand { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Series { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Year { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Fuel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Gear { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int KM { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int EnginePower { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int EngineCapacity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Door { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
